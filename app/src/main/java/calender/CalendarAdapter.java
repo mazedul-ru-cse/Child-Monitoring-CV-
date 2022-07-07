1e5ed3b9fc29907773589ea2a04c9f193fa07a4e
Mazed <mazedulislam4970@gmail.com>
2022-07-07 06:07:51 +0600

    Ready to release

--- app/src/main/java/calender/CalendarAdapter.java
+++ app/src/main/java/calender/CalendarAdapter.java
@@ -1,20 +1,14 @@
 package calender;
 
 import android.graphics.Color;
-import android.graphics.DashPathEffect;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.example.childmonitoringchildversion.MainActivity;
 import com.example.childmonitoringchildversion.R;
-
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
